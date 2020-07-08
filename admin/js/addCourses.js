80be3e66ecdab6b0a977648602eb35f9bca097cc
Rohit-Mundada <rohitrm0801@gmail.com>
2020-07-08 11:28:08 +0530

    add-courses.html update

--- admin/js/addCourses.js
+++ admin/js/addCourses.js
@@ -1,8 +1,16 @@
 $(document).ready(function () {
   $('.sel').chosen({ width: '300px' });
-})
+});
 
 $('#myForm').on('submit', function (e) {
   $('#successModal').modal('show');
   e.preventDefault();
 });
+
+// Request to add a new course to backend
+document
+  .getElementById('add-course-button')
+  .addEventListener('click', async () => {
+    try {
+    } catch (err) {}
+  });
