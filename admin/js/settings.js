eb8412ea235a61b5864d9b4501ba8b65c486f4d8
Rohit-Mundada <rohitrm0801@gmail.com>
2020-07-11 00:18:00 +0530

    Updated change password request

--- admin/js/settings.js
+++ admin/js/settings.js
@@ -1,32 +1,33 @@
-// // Settings request
-// document
-//   .getElementById("change-password")
-//   .addEventListener("click", async () => {
-//     try {
-//       const password = document.querySelector("#new-password").value;
-//       const confirmPassword = document.querySelector("#confirm-password").value;
+// Settings request
+document
+  .getElementById("change-password")
+  .addEventListener("click", async () => {
+    try {
+      const password = document.querySelector("#new-password").value;
+      const confirmPassword = document.querySelector("#confirm-password").value;
 
-//       if (password === confirmPassword) {
-//         const response = await fetch(`${url}/user/changepasswordinlogin`, {
-//           method: "POST",
-//           headers: {
-//             token: sessionStorage.getItem("token"),
-//             "Content-Type": "application/json",
-//           },
-//           body: { newpass: password }
-//         });
+      if (password === confirmPassword) {
+        const response = await fetch(`${url}/user/changepasswordinlogin`, {
+          method: "POST",
+          headers: {
+            token: `${sessionStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ newpass: password }),
+        });
 
-//         const data = response.json();
+        const data = await response.json();
 
-//         if (data.ok) {
-//           alert('Password changed successfully!');
-//         } else {
-//           alert('Some server error, reload browser');
-//         }
-//       } else {
-//         document.querySelector("#error-message").style.display = "block";
-//       }
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   });
+        console.log(data);
+        if (data.ok) {
+          alert("Password changed successfully!");
+        } else {
+          alert("Some server error, reload browser");
+        }
+      } else {
+        document.querySelector("#error-message").style.display = "block";
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
