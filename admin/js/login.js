c7251dd6909e4a2bfd8030f23783e9199d238fb2
sanikachepe <sanikachepe.potterhead@gmail.com>
2020-07-15 13:58:21 +0530

    added show password to login

--- admin/js/login.js
+++ admin/js/login.js
@@ -1,13 +1,13 @@
 // Login Request
-document.getElementById('login-button').addEventListener('click', async (e) => {
+document.getElementById("login-button").addEventListener("click", async (e) => {
   try {
     e.preventDefault();
-    const email = document.querySelector('#email').value;
-    const password = document.querySelector('#password').value;
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
 
     const response = await fetch(`${url}/user/login`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         email,
         password,
@@ -17,13 +17,23 @@ document.getElementById('login-button').addEventListener('click', async (e) => {
     const data = await response.json();
 
     if (data.ok) {
-      sessionStorage.setItem('token', data.token);
-      window.location.href = '/dashboard.html';
+      sessionStorage.setItem("token", data.token);
+      window.location.href = "/dashboard.html";
     } else {
-      document.querySelector('.error-message-alert').style.display = 'block';
+      document.querySelector(".error-message-alert").style.display = "block";
     }
   } catch (err) {
-    document.querySelector('.error-message').innerHTML =
-      'Some error occured. Refresh browser and try again';
+    document.querySelector(".error-message").innerHTML =
+      "Some error occured. Refresh browser and try again";
+  }
+});
+
+$(".toggle-password").click(function () {
+  $(this).toggleClass("fa-eye fa-eye-slash");
+  var input = $("#password-field");
+  if (input.attr("type") === "password") {
+    input.attr("type", "text");
+  } else {
+    input.attr("type", "password");
   }
 });
