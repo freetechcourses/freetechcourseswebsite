d30dc050b8c8b5084ea4aa0bd0cf2e3cdeb20bb4
Rohit-Mundada <rohitrm0801@gmail.com>
2020-07-16 19:19:00 +0530

    Completed Message router and form

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,28 @@
 // Sending Message data to backend
-document.getElementById('submit-message').addEventListener('click', async (e) => {
+document
+  .getElementById('submit-message')
+  .addEventListener('click', async (e) => {
     try {
-        e.preventDefault();
-        const name = document.querySelector('#name').value;
-        const email = document.querySelector('#email').value;
-        const message = document.querySelector('#message').value;
+      e.preventDefault();
+      const name = document.querySelector('#name').value;
+      const email = document.querySelector('#email').value;
+      const message = document.querySelector('#message').value;
 
-        const response = await fetch(`${url}/contact/newcontact`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ name, email, message })
-        });
+      const response = await fetch(`${url}/contact/newcontact`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name, email, message }),
+      });
 
-        console.log(response);
+      response.ok
+        ? (document.querySelector(
+            '.sent-message-successful-alert'
+          ).style.display = 'block')
+        : (document.querySelector('.sent-message-error-alert').style.display =
+            'block');
     } catch (err) {
-        console.log(err);
+      console.log(err);
+      document.querySelector('.sent-message-error-alert').style.display =
+        'block';
     }
-})
+  });
