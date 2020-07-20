317d53f5326781c350098c70bce703d6be1193b1
Rohit-Mundada <rohitrm0801@gmail.com>
2020-07-20 10:26:30 +0530

    Messages displayed in admin panel, added delete functionality

--- admin/js/deleteCourses.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  $('.sel').chosen({ width: '400px' });
-})
-
-$('#myForm').on('submit', function (e) {
-  $('#deleteModal').modal('show');
-  e.preventDefault();
-});
