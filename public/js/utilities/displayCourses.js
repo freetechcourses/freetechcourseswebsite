dca00a43ff269485d75f4af46f67e2a63435d3bd
Rohit-Mundada <rohitrm0801@gmail.com>
2020-07-21 07:57:12 +0530

    Refactored main page js

---
+++ public/js/utilities/displayCourses.js
@@ -0,0 +1,65 @@
+const displayCourses = (data) => {
+  for (let i = 0; i < data.length; i++) {
+    const courseCard = `<div class="col-lg-4 col-sm-6 mb-4">
+                            <div class="card" id=${data[i]._id} data-target="#details" data-toggle="modal">
+                              <img class="card-img-top" src=${data[i].courseImage} alt=${data[i].name} />
+                              <div class="card-body">
+                                <h5 class="card-title course-name">${data[i].name}</h5>
+                                <p class="card-text">
+                                  ${data[i].description}
+                                </p>
+                                <p>
+                                  <a href=${data[i].hyperlink} target="_blank" class="card-link">
+                                    <strong>Course link</strong><br />
+                                  </a>
+                                </p>
+                              </div>
+                            </div>
+                          </div>`;
+    $('#display-courses').append(courseCard);
+
+    // Getting more info about a individual course in a modal
+    document
+      .getElementById(`${data[i]._id}`)
+      .addEventListener('click', async () => {
+        try {
+          const response = await (
+            await fetch(`${url}/course/single/${data[i]._id}`, {
+              method: 'GET',
+            })
+          ).json();
+
+          const detailsInfo = `<img 
+                                    alt=${response.data.name} 
+                                    class="img-fluid" 
+                                    src=${response.data.courseImage}
+                                  />
+                                  <a href=${
+                                    response.data.hyperlink
+                                  } target="_blank" class="card-link">
+                                    <h3 class="pt-4">${response.data.name}</h3>
+                                  </a>
+                                  <p class="lead">
+                                    ${response.data.description}
+                                  </p>
+                                  <p>
+                                    <strong>Date:</strong> 
+                                    ${new Date(
+                                      response.data.date
+                                    ).toDateString()}
+                                  </p>
+                                  <p>
+                                    <a href=${
+                                      response.data.hyperlink
+                                    } target="_blank" class="card-link">
+                                      <strong>Course link</strong> 
+                                    </a>
+                                  </p>
+                                  `;
+          $('#details-body').html(detailsInfo);
+        } catch (err) {
+          console.log(err);
+        }
+      });
+  }
+};
