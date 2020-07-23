18eeb26ba53e1567947749c403b258733a2e05c5
Rohit-Mundada <rohitrm0801@gmail.com>
2020-07-23 10:52:19 +0530

    Pagination completed

--- public/js/utilities/counter.js
+++ public/js/utilities/counter.js
@@ -1,12 +1,3 @@
-{
-  /* <p>
-  <strong>Languages:</strong>$
-  {courseDetailsResponse.data.languages.map((language) =>
-    console.log(language)
-  )}
-</p>; */
-}
-
 function a() {
   let count = 0;
   function increment() {
