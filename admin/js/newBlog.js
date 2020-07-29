75f4d3755d2fba1a9faa80f9bb900874ecc8cd1f
sanikachepe <sanikachepe.potterhead@gmail.com>
2020-07-29 16:06:04 +0530

    markup done!

--- admin/js/newBlog.js
+++ admin/js/newBlog.js
@@ -1,28 +1,28 @@
 // Adding new blog request
 document
-  .getElementById('add-blog-button')
-  .addEventListener('click', async (e) => {
+  .getElementById("add-blog-button")
+  .addEventListener("click", async (e) => {
     try {
       e.preventDefault();
-      const title = document.getElementById('title').value;
-      const blogImage = document.getElementById('img-link').value;
-      const body = document.getElementById('body').value;
+      const title = document.getElementById("title").value;
+      const blogImage = document.getElementById("img-link").value;
+      const body = `<div>${quill.root.innerHTML}</div>`;
       const date = Date.now();
 
       const response = await (
         await fetch(`${url}/blog/add`, {
-          method: 'POST',
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
-            token: `${sessionStorage.getItem('token')}`,
+            "Content-Type": "application/json",
+            token: `${sessionStorage.getItem("token")}`,
           },
           body: JSON.stringify({ title, body, blogImage, date }),
         })
       ).json();
 
       if (response.ok) {
-        $('#add-blog-form').on('submit', function (e) {
-          $('#successModal').modal('show');
+        $("#add-blog-form").on("submit", function (e) {
+          $("#successModal").modal("show");
           e.preventDefault();
         });
       }
